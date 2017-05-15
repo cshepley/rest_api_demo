d21f75c82189a73356e2ceef79faf210290e27df
Chris Shepley <cshepley@gmail.com>
2017-05-15 12:46:55 -0400

    Update models.py
    Reworking for widget model specified.

--- rest_api_demo/database/models.py
+++ rest_api_demo/database/models.py
@@ -7,25 +7,25 @@ from datetime import datetime
 from rest_api_demo.database import db
 
 
-class Post(db.Model):
+class Widget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    body = db.Column(db.Text)
-    pub_date = db.Column(db.DateTime)
+    name = db.Column(db.String(80))
+    quantity = db.Column(db.Integer)
+    create_date = db.Column(db.DateTime)
 
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    category = db.relationship('Category', backref=db.backref('posts', lazy='dynamic'))
+    category = db.relationship('Category', backref=db.backref('widgets', lazy='dynamic'))
 
-    def __init__(self, title, body, category, pub_date=None):
-        self.title = title
-        self.body = body
-        if pub_date is None:
-            pub_date = datetime.utcnow()
-        self.pub_date = pub_date
+    def __init__(self, name, quantity, category, create_date=None):
+        self.name = name
+        self.quantity = quantity
+        if create_date is None:
+            create_date = datetime.utcnow()
+        self.pub_date = create_date
         self.category = category
 
     def __repr__(self):
-        return '<Post %r>' % self.title
+        return '<Post %r>' % self.name
 
 
 class Category(db.Model):
